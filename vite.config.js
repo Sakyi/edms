73d13e3a70ef8492d589c94152a1b33c650d1f48
kobilinux <sakyiowusu@gmail.com>
2026-03-10 09:40:55 +0000

    added base key to vite.config.js file and imported process function from process module

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import process from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: process.env.VITE_BASE_PATH || "/edms",
 });
